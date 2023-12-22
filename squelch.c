48f164ec6938bf86f2e7efbfa670034216367a9a
Michael <quadricopter@gmail.com>
2023-12-22 14:11:43 +0100

    Display squelch gate status (opened/closed)

--- squelch.c
+++ squelch.c
@@ -5,20 +5,28 @@
 #include <string.h>
 #include <complex.h>
 #include <getopt.h>
+#include <time.h>
+#include <sys/time.h>
 
 #define DEFAULT_SAMPLES_COUNT   1024
 #define DEFAULT_SQUELCH_LEVEL   0.01
 
+typedef enum {
+    SQUELCH_STATUS_CLOSE,
+    SQUELCH_STATUS_OPEN
+} t_squelch_status;
 
 int main(int argc, char *argv[])
 {
-    float complex  *iq_in = NULL;
-    float complex  *iq_zero = NULL;
-    int             sample_count = DEFAULT_SAMPLES_COUNT;
-    float           squelch_level = DEFAULT_SQUELCH_LEVEL;
-    int             c;
-    bool            blocking = false;
-    bool            dump = false;
+    float complex      *iq_in = NULL;
+    float complex      *iq_zero = NULL;
+    int                 sample_count = DEFAULT_SAMPLES_COUNT;
+    float               squelch_level = DEFAULT_SQUELCH_LEVEL;
+    bool                blocking = false;
+    bool                dump = false;
+    t_squelch_status    current_squelch_status = SQUELCH_STATUS_CLOSE;
+    t_squelch_status    previous_squelch_status = SQUELCH_STATUS_CLOSE;
+    struct timeval      tv_open;
 
     fprintf(stderr, "[SQUELCH] sizeof(float): %ld\n", sizeof(float));
     fprintf(stderr, "[SQUELCH] sizeof(float complex): %ld\n", sizeof(float complex));
@@ -29,6 +37,7 @@ int main(int argc, char *argv[])
 
     while (1) {
 
+        int c;
         int option_index = 0;
         static struct option long_options[] = {
             {"blocking", no_argument,       0,  'b' },
@@ -44,12 +53,6 @@ int main(int argc, char *argv[])
             break;
 
         switch (c) {
-        case 0:
-            fprintf(stderr, "[SQUELCH] option %s", long_options[option_index].name);
-            if (optarg)
-                fprintf(stderr, " with arg %s", optarg);
-            fprintf(stderr, "\n");
-            break;
 
         case 'b':
             blocking = true;
@@ -80,11 +83,8 @@ int main(int argc, char *argv[])
      */
 
     iq_in = malloc(sample_count * sizeof(float complex));
-    if (blocking == false) {
-
-        iq_zero = malloc(sample_count * sizeof(float complex));
-        memset(iq_zero, 0, sample_count * sizeof(float complex));
-    }
+    iq_zero = malloc(sample_count * sizeof(float complex));
+    memset(iq_zero, 0, sample_count * sizeof(float complex));
 
     /*
      * Read complex samples
@@ -106,10 +106,49 @@ int main(int argc, char *argv[])
             fprintf(stderr, "[SQUELCH] mean_power_level: %f\n", mean_power_level);
 
         /* stdout gate */
-        if (mean_power_level > DEFAULT_SQUELCH_LEVEL)
+        if (mean_power_level > squelch_level) {
+
+            current_squelch_status = SQUELCH_STATUS_OPEN;
             fwrite(iq_in, sizeof(float complex), sample_count, stdout);
-        else if (blocking == false)
-            fwrite(iq_zero, sizeof(float complex), sample_count, stdout);
+        }
+        else {
+
+            /* Flush last samples before mute */
+            if (current_squelch_status == SQUELCH_STATUS_OPEN)
+                fwrite(iq_in, sizeof(float complex), sample_count, stdout);
+
+            /* Mute */
+            if (blocking == false)
+                fwrite(iq_zero, sizeof(float complex), sample_count, stdout);
+
+            current_squelch_status = SQUELCH_STATUS_CLOSE;
+        }
+
+        /* Dump gate status */
+        if (previous_squelch_status != current_squelch_status) {
+
+            if (current_squelch_status == SQUELCH_STATUS_OPEN) {
+
+                struct tm  *timeinfo;
+                char        time_str[32];
+
+                gettimeofday(&tv_open, NULL);
+                timeinfo = localtime(&tv_open.tv_sec);
+                strftime(time_str, 32, "%Y-%m-%d %H:%M:%S", timeinfo);
+                fprintf(stderr, "[SQUELCH] Opened at %s\n", time_str);
+            }
+            else {
+
+                struct timeval  tv_close;
+                struct timeval  tv_diff;
+
+                gettimeofday(&tv_close, NULL);
+                timersub(&tv_close, &tv_open, &tv_diff);
+                fprintf(stderr, "[SQUELCH] Closed after %ld.%06lds\n", tv_diff.tv_sec, tv_diff.tv_usec);
+            }
+        }
+
+        previous_squelch_status = current_squelch_status;
     }
 
     /*
@@ -117,8 +156,7 @@ int main(int argc, char *argv[])
      */
 
     free(iq_in);
-    if (iq_zero != NULL)
-        free(iq_zero);
+    free(iq_zero);
 
     return EXIT_SUCCESS;
 }
